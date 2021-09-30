e853e2303d170e4de5564db8c10f14b66cc22488
Tijmen Mulder <tmulder@cloudsuite.com>
2021-09-30 21:19:37 +0200

    editor panes and localstorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 import './App.css'
 
@@ -80,98 +80,15 @@ const GridRenderer = ({ data, onDrag, onDragOver, onDragLeave, onDragEnd, onDrop
 
 const App = () => {
 
-  const [ data, setData ] = useState(
-    [
-      {
-        "component": {
-          "data": {
-            "name": "Item 1"
-            },
-            "type": "BasicWidget"
-        },
-        "type": "component",
-        "id": crypto.randomUUID()
-      },
-      {
-        "type": "row",
-        "id": crypto.randomUUID(),
-        "children": [
-          {
-            "type": "column",
-            "id": crypto.randomUUID(),
-            "children": []
-          },
-          {
-            "type": "column",
-            "id": crypto.randomUUID(),
-            "children": []
-          }
-        ]
-      },
-      {
-        "type": "row",
-        "id": crypto.randomUUID(),
-        "children": [
-          {
-            "type": "column",
-            "id": crypto.randomUUID(),
-            "children": [
-              {
-                "component": {
-                  "data": {
-                    "name": "Item 2"
-                    },
-                    "type": "BasicWidget"
-                },
-                "type": "component",
-                "id": crypto.randomUUID()
-              },
-              {
-                "type": "row",
-                "id": crypto.randomUUID(),
-                "children": [
-                  {
-                    "type": "column",
-                    "id": crypto.randomUUID(),
-                    "children": [
-                      {
-                        "component": {
-                          "data": {
-                            "name": "Item 3"
-                            },
-                            "type": "BasicWidget"
-                        },
-                        "type": "component",
-                        "id": crypto.randomUUID()
-                      }
-                    ]
-                  },
-                  {
-                    "type": "column",
-                    "id": crypto.randomUUID(),
-                    "children": []
-                  },
-                  {
-                    "type": "column",
-                    "id": crypto.randomUUID(),
-                    "children": []
-                  }
-                ]
-              }
-            ]
-          },
-          {
-            "type": "column",
-            "id": crypto.randomUUID(),
-            "children": []
-          }
-        ]
-      }
-    ]
-  )
-
+  const [ data, setData ] = useState([])
   const [ target, setTarget ] = useState(undefined)
+  const [ activeItem, setActiveItem ] = useState(undefined)
+
   const insertIndicator = useRef(null)
+
+  useEffect(() => {
+    setData(JSON.parse(localStorage.getItem('layout')))
+  }, [])
 
   const onDrag = (event) => {
     setTarget(event.target)
@@ -251,15 +168,27 @@ const App = () => {
     }
 
     onDragEnd(event)
+
+    localStorage.setItem('layout',  JSON.stringify(data))
+
     setData([...data])
   }
 
   const onClick = (event) => {
-    if (event.target.classList.contains('row')) {
-      const res = findItem(data, event.target.dataset.id)
-      res.items[res.index].children.push({ type: 'column', id: crypto.randomUUID(), children: [] })
-      setData([...data])
+    const res = findItem(data, event.target.dataset.id)
+    const item = res.items[res.index]
+
+    if (item.type === 'component') {
+
+    } else if (item.type === 'row') {
+
     }
+
+    // if (event.target.classList.contains('row')) {
+    //   const res = findItem(data, event.target.dataset.id)
+    //   res.items[res.index].children.push({ type: 'column', id: crypto.randomUUID(), children: [] })
+    //   setData([...data])
+    // }
   }
 
   const _findItem = (item, id) => {
@@ -292,18 +221,26 @@ const App = () => {
 
   return (
     <div className="App">
-      <GridRenderer
-        data={data}
-        onDrag={onDrag}
-        onDragOver={onDragOver}
-        onDragLeave={onDragLeave}
-        onDragEnd={onDragEnd}
-        onDrop={onDrop}
-        onClick={onClick}
-      />
-      <div className="insert-indicator" ref={insertIndicator}></div>
+      <div className="editor">
+        <div className="layout-pane">
+          <GridRenderer
+            data={data}
+            onDrag={onDrag}
+            onDragOver={onDragOver}
+            onDragLeave={onDragLeave}
+            onDragEnd={onDragEnd}
+            onDrop={onDrop}
+            onClick={onClick}
+          />
+          <div className="insert-indicator" ref={insertIndicator}></div>
+        </div>
+        <div className="properties-pane"></div>
+      </div>
     </div>
   )
 }
 
 export default App
+
+
+// [{"component":{"data":{"name":"Item 3"},"type":"BasicWidget"},"type":"component","id":"a09e99a6-121a-42b3-ab60-bb6cad7545f4"},{"type":"row","id":"fe41a012-8079-4458-bf38-7f4d07b6a05b","children":[{"type":"column","id":"4a1b25a2-7780-4bc8-90eb-808202802825","children":[]},{"type":"column","id":"ac62583f-cc0a-43c7-849e-f5696b3a44f5","children":[{"type":"row","id":"cb875c3b-1053-4800-99fa-fd03d211594f","children":[{"type":"column","id":"28aa1a9b-7604-4d1a-a738-f5b75110c8c6","children":[{"component":{"data":{"name":"Item 1"},"type":"BasicWidget"},"type":"component","id":"8d12f736-bc11-4a03-8c83-95c3a3b3e982"}]},{"type":"column","id":"1a704410-d2a4-414c-9a78-d26d1fd6a393","children":[{"component":{"data":{"name":"Item 2"},"type":"BasicWidget"},"type":"component","id":"58283438-460b-4f56-8ea1-dc5a8775d9f4"}]}]}]},{"type":"column","id":"ebb71138-112c-4e2d-8466-2333cdb9cc1d","children":[]}]},{"type":"row","id":"a7140653-1eb7-4766-8132-32772bbc5893","children":[{"type":"column","id":"c4d05324-b33a-464c-80e2-796a7e329467","children":[]},{"type":"column","id":"e96787b6-bdaf-4dcf-9ada-de699d2e00fc","children":[]}]}]
